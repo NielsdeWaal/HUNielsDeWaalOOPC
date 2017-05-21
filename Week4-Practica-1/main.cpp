add2315a7a78bef2c88bfda319e77ade5584e373
Niels de Waal <NielsdeWaal@users.noreply.github.com>
2017-05-21 20:20:57 +0200

    Opdrachten week 4 af.

---
+++ Week4-Practica-1/main.cpp
@@ -0,0 +1,60 @@
+#include "hwlib.hpp"
+
+//$Id$
+
+/**
+ * @file main.cpp
+ * @date 20/5/17
+ * @author Niels de Waal
+ * Jorn Bunk
+ * @brief Code voor practicum week 4 opdracht 1
+ * 
+ * Code voor het laten branden van een rij led lampjes, knight rider style.
+ */
+
+/**
+ * @brief main functie.
+ * @return 0 bij goed verloop.
+ */
+
+int main( void ){	
+	// kill the watchdog
+	WDT->WDT_MR = WDT_MR_WDDIS;
+
+	namespace target = hwlib::target;
+
+	auto led0 = hwlib::target::pin_out(target::pins::d7);
+	auto led1 = hwlib::target::pin_out(target::pins::d6);
+	auto led2 = hwlib::target::pin_out(target::pins::d5);
+	auto led3 = hwlib::target::pin_out(target::pins::d4);
+	
+	for(;;) {
+		
+		led0.set(1);
+		led1.set(1);
+		
+		hwlib::wait_ms( 200 ); 
+		
+		led0.set(0);
+		led2.set(1);
+		
+		hwlib::wait_ms( 200 ); 
+		
+		led1.set(0);
+		led3.set(1);
+		
+		hwlib::wait_ms( 200 ); 
+		
+		led3.set(0);
+		led1.set(1);
+		
+		hwlib::wait_ms( 200 ); 
+		
+		led2.set(0);
+		led1.set(1);
+		led0.set(1);
+		
+	}
+	
+	return 0;
+}
---
+++ Week4-Practica-2/main.cpp
@@ -0,0 +1,96 @@
+/**
+ * @file main.cpp
+ * @date 21/5/17
+ * @author Niels de Waal
+ * Jorn Bunk
+ * @brief Code voor practicum week 4 opdracht 2
+ * 
+ * Code voor het laten branden van een rij led lampjes.
+ * Deze worden aan de hand van een counter (die door knopjes worden bestuurd), 
+ * incrementeel aangestuurd.
+ */
+
+#include "hwlib.hpp"
+
+/**
+ * @brief Functie voor het incrementeel aansturen van ledjes.
+ * 
+ * Wait functies worden gebruikt voor het debouncen van de knoppen.
+ * 
+ * @param leds
+ * @param up
+ * @param down
+ */
+void counter(hwlib::port_out & leds, hwlib::pin_in & up, hwlib::pin_in & down) {
+	bool upHigh = false;
+	bool downHigh = false;
+	
+	int counter = 0;
+	while(1) {
+	if(up.get() && !upHigh) {
+		hwlib::wait_ms( 200 );
+		if (counter == 4) {
+			counter = 4;
+		} else {
+			counter++;
+		}
+		upHigh = true;
+	} else if (down.get() && !downHigh) {
+		hwlib::wait_ms( 200 );
+		if (!counter) {
+			counter = 0;
+		} else {
+			counter--;
+		}
+		downHigh = true;
+	} else if (!down.get() && upHigh) {
+		hwlib::wait_ms( 200 );
+		upHigh = !upHigh;
+	} else if (!up.get() && downHigh) {
+		hwlib::wait_ms( 200 );
+		downHigh = !downHigh;
+	}
+	
+	if (counter == 0) {
+		leds.set(0);
+	} else if (counter == 1) {
+		leds.set(1);
+	} else if (counter == 2) {
+		leds.set(3);
+	} else if (counter == 3) {
+		leds.set(7);
+	} else if (counter == 4) {
+		leds.set(15);
+	}
+	
+	}
+}
+
+/**
+ * @brief Main functie 
+ * 
+ * Alle parameters worden opgezet.
+ * Hierna word de functie aangeroepen voor het uitvoeren van de opdracht.
+ */
+
+int main( void ){	
+	// kill the watchdog
+	WDT->WDT_MR = WDT_MR_WDDIS;
+
+	namespace target = hwlib::target;
+
+	auto led = target::pin_in_out(1, 27);
+	led.direction_set_output();
+
+	auto led0 	= target::pin_out(target::pins::d7);
+	auto led1 	= target::pin_out(target::pins::d6);
+	auto led2 	= target::pin_out(target::pins::d5);
+	auto led3 	= target::pin_out(target::pins::d4);
+	
+	auto leds 	= hwlib::port_out_from_pins(led0, led1, led2, led3);
+	
+	auto up 	= target::pin_in(target::pins::d3);
+	auto down 	= target::pin_in(target::pins::d2);
+	
+	counter(leds, up, down);
+}
